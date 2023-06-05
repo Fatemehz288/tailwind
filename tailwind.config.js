62a431ec039de70d136cb10484e0a04a1b6f3eb7
Fatemehz288 <118839741+Fatemehz288@users.noreply.github.com>
2023-06-05 09:39:27 -0700

    Add file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        red: "#f40361",
+        black: "#141414",
+      },
+    },
+  },
+  plugins: [],
+}
+
